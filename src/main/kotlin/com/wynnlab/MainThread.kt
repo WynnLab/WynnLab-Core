c23c32b0be4cc2e650c45497e4766858a2147ef4
FauxKiwi <sfn14ok04@gmail.com>
2021-04-08 13:52:21 +0200

    Added walk speed jump height

--- src/main/kotlin/com/wynnlab/MainThread.kt
+++ src/main/kotlin/com/wynnlab/MainThread.kt
@@ -4,6 +4,8 @@ import com.wynnlab.api.getId
 import com.wynnlab.api.standardActionBar
 import org.bukkit.Bukkit
 import org.bukkit.attribute.Attribute
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 object MainThread : Runnable {
     @Volatile
@@ -14,15 +16,33 @@ object MainThread : Runnable {
         val s4 = tick % 80 == 0
 
         for (player in Bukkit.getServer().onlinePlayers) {
+            // Send action bar
             player.standardActionBar()
+
+            // Reset exhaustion to not lose mana except in spell casts
             player.exhaustion = 0f
+
+            // Store max health and set attribute
             val maxHealth = (505 + player.getId("health_bonus")).coerceIn(1, 1000000).toDouble()
             player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue = maxHealth
-            if (s1) player.foodLevel = (player.foodLevel + 1).coerceAtMost(20)
+
+            if (s1) {
+                // Natural mana regen
+                player.foodLevel = (player.foodLevel + 1).coerceAtMost(20)
+
+                // Jump height
+                val jumpHeight = player.getId("jump_height")
+                if (jumpHeight != 0) player.addPotionEffect(PotionEffect(PotionEffectType.JUMP, 21, jumpHeight - 1, true, false, false))
+            }
+
+            // Mana regen and health regen
             if (s4) {
                 player.foodLevel = (player.foodLevel + player.getId("mana_regen")).coerceAtMost(20)
                 player.health = (player.health + player.getId("health_regen_raw") * (1 + player.getId("health_regen") / 100f)).coerceAtMost(maxHealth)
             }
+
+            // Walk speed
+            player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)!!.baseValue = .1 + player.getId("speed") * .001
         }
 
         ++tick
