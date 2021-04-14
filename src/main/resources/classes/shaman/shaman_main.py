5c3e292b39f1f8bb73a7250a1a40d2c006314d68
FauxKiwi <sfn14ok04@gmail.com>
2021-04-14 20:26:15 +0200

    Added shaman auto attack and Totem spell

--- src/main/resources/classes/shaman/shaman_main.py
+++ src/main/resources/classes/shaman/shaman_main.py
@@ -3,12 +3,62 @@ from org.bukkit import Particle, Sound
 from com.wynnlab.spells import PySpell
 
 class Spell(PySpell):
+    def __init__(self):
+        self.p_left = None
+        self.p_center = None
+        self.p_right = None
+
+        self.dir_left = None
+        self.dir_center = None
+        self.dir_right = None
+
+        self.hit = set()
+
+    def init(self):
+        self.p_left = self.player.getEyeLocation().clone()
+        self.p_center = self.p_left.clone()
+        self.p_right = self.p_center.clone()
+
+        self.dir_center = self.player.getEyeLocation().getDirection().clone()
+        self.dir_left = self.dir_center.clone().rotateAroundY(-.3)
+        self.dir_right = self.dir_center.clone().rotateAroundY(.3)
+
     def tick(self):
-        self.sound(Sound.ENTITY_PLAYER_ATTACK_SWEEP, 1, .8)
-        l = self.player.getEyeLocation().clone().add(self.player.getEyeLocation().getDirection())
-        self.particle(l, Particle.SWEEP_ATTACK, 1, 0, 0, 0, 0)
+        self.p_left.add(self.dir_left)
+        self.p_center.add(self.dir_center)
+        self.p_right.add(self.dir_right)
 
-        for e in self.nearbyMobs(l, 2, 2, 2):
-            self.damage(e, 2)
+        if self.t == 0:
+            self.sound(Sound.ENTITY_PLAYER_ATTACK_SWEEP, 1, .6)
+            self.sound(Sound.ENTITY_ZOMBIE_INFECT, .5, 1.8)
+            self.sound(Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK, 1, 1.4)
+            if self.clone:
+                self.sound(Sound.ENTITY_FIREWORK_ROCKET_LAUNCH, .5, .2)
 
-# THIS IS ONLY A PLACEHOLDER
+        else:
+            self.particles(self.p_left)
+            self.particles(self.p_center)
+            self.particles(self.p_right)
+
+        for e in self.nearbyMobs(self.p_left, 1, 2, 1):
+            if e in self.hit:
+                continue
+            self.hit.add(e)
+            self.damage(e, 1.5)
+        for e in self.nearbyMobs(self.p_center, 1, 2, 1):
+            if e in self.hit:
+                continue
+            self.hit.add(e)
+            self.damage(e, 1.5)
+        for e in self.nearbyMobs(self.p_right, 1, 2, 1):
+            if e in self.hit:
+                continue
+            self.hit.add(e)
+            self.damage(e, 1.5)
+
+    def particles(self, l):
+        if self.clone:
+            self.particle(l, Particle.SPELL_MOB, 0, 1, 1, 1, 1)
+        else:
+            self.particle(l, Particle.CRIT_MAGIC, 3, 0, 0, 0, 0)
+        #self.particle(l, Particle.CRIT, 2, 0, 0, 0, 0)
--- src/main/resources/classes/shaman/totem.py
+++ src/main/resources/classes/shaman/totem.py
@@ -1,14 +1,112 @@
-from org.bukkit import Particle, Sound
+from org.bukkit import Material, Particle, Sound
+from org.bukkit.entity import EntityType, Mob, Player
+from org.bukkit.inventory import ItemStack
 
+from com.wynnlab.api import PersistentDataAPI
 from com.wynnlab.spells import PySpell
 
+from java.lang import Math
+
 class Spell(PySpell):
+    def __init__(self):
+        self.totem = None
+        self.holo = None
+
+        self.hit = False
+
     def tick(self):
-        self.sound(Sound.ENTITY_PLAYER_ATTACK_SWEEP, 1, .8)
-        l = self.player.getEyeLocation().clone().add(self.player.getEyeLocation().getDirection())
-        self.particle(l, Particle.SWEEP_ATTACK, 1, 0, 0, 0, 0)
+        if self.t == 0:
+            self.player.addScoreboardTag('totem')
 
-        for e in self.nearbyMobs(l, 2, 2, 2):
-            self.damage(e, 2)
+            self.sound(Sound.ENTITY_PLAYER_ATTACK_WEAK, .8, .3)
+            self.sound(Sound.ENTITY_IRON_GOLEM_HURT, 1, .8)
+            if self.clone:
+                self.sound(Sound.ENTITY_BLAZE_SHOOT, 1, .9)
+                self.sound(Sound.ENTITY_BLAZE_AMBIENT, .1, .7)
 
-# THIS IS ONLY A PLACEHOLDER
+            dir = self.player.getEyeLocation().getDirection().clone().setY(self.player.getEyeLocation().getDirection().getY())
+
+            self.totem = self.player.getWorld().spawnEntity(self.player.getLocation().clone().add(0, 1, 0).add(dir), EntityType.ARMOR_STAND)
+
+            self.totem.addScoreboardTag('totem')
+            self.totem.setGravity(True)
+            self.totem.setInvulnerable(True)
+            self.totem.setVisible(True)
+
+            totem_item = ItemStack(Material.STONE_SHOVEL)
+            totem_meta = totem_item.getItemMeta()
+            totem_meta.setUnbreakable(True)
+            totem_meta.setDamage(29 if self.clone else 28)
+            totem_item.setItemMeta(totem_meta)
+
+            self.totem.getEquipment().setHelmet(totem_item)
+
+            self.totem.setVelocity(dir.setY(1))
+
+            #PersistentDataAPI.setInt(PersistentDataAPI.getData(self.player), 'totem', self.totem.getEntityID())
+
+        if not self.hit:
+            if not self.totem.isOnGround():
+                self.particle(self.totem.getLocation().clone().add(0, 1, 0), Particle.END_ROD if self.clone else Particle.VILLAGER_HAPPY, 1, 0, 0, 0, 0)
+                self.t = 0
+
+            else:
+                self.hit = True
+
+                self.totem.setMarker(True)
+
+                self.holo = self.player.getWorld().spawnEntity(self.totem.getLocation().clone().add(0, 2.7, 0), EntityType.ARMOR_STAND)
+
+                self.holo.setMarker(True)
+                self.holo.setVisible(False)
+                self.holo.setInvulnerable(True)
+                self.holo.setCustomName(u'§c20s')
+                self.holo.setCustomNameVisible(True)
+
+                #PersistentDataAPI.setInt(PersistentDataAPI.getData(self.player), 'totem_holo', self.holo.getEntityID())
+
+                l = self.totem.getLocation()
+
+                self.particle(l.clone().add(0,-1,0), Particle.EXPLOSION_HUGE, 1, 0, 0, 0, 1)
+                self.particle(l, Particle.FIREWORKS_SPARK if self.clone else Particle.CRIT_MAGIC, 5, 2, 2, 2, .8)
+                self.particle(l, Particle.SQUID_INK, 5, 2, 2, 2, .5)
+
+                self.sound(l, Sound.ENTITY_EVOKER_PREPARE_SUMMON, .9, 1.1)
+                self.sound(l, Sound.ENTITY_IRON_GOLEM_DEATH, 1, .8)
+                self.sound(l, Sound.ITEM_TOTEM_USE, .9, 1)
+                if self.clone:
+                    self.sound(l, Sound.ENTITY_FIREWORK_ROCKET_BLAST_FAR, 1, .7)
+                    self.sound(l, Sound.BLOCK_FIRE_EXTINGUISH, .5, .5)
+                    self.sound(l, Sound.ENTITY_BLAZE_DEATH, .3, 1)
+
+                for e in self.nearbyMobs(l, 4, 4, 4):
+                    self.damage(e, 4)
+
+        else:
+            if self.totem.isOnGround() and self.t >= 20:
+                self.totem.setMarker(True)
+
+            if self.t < 400:
+                if self.t % 20 == 0:
+                    self.holo.setCustomName(u'§c{0}s'.format((400 - self.t) // 20))
+
+                    for e in self.player.getWorld().getNearbyEntities(self.totem.getLocation(), 8, 4, 8):
+                        if not isinstance(e, Mob):
+                            continue
+
+                        if isinstance(e, Player):
+                            pass # Heal
+
+                        else:
+                            self.damage(e, 1)
+
+                l = self.totem.getLocation().clone().add(Math.sin(self.t * 10 * DEG2RAD) * 8, .7, Math.cos(self.t * 10 * DEG2RAD) * 8)
+                self.particle(l, Particle.FIREWORKS_SPARK if self.clone else Particle.TOTEM, 1, 0, 0, 0, 0)
+
+                self.holo.teleport(self.totem.getLocation().clone().add(0, 2.7, 0))
+
+            else:
+                self.totem.remove()
+                self.holo.remove()
+
+                self.player.removeScoreboardTag('totem')
